45341a229050278f08c47f92a1d44df86ab0fc9e
Isaac Johnston <isaac@smxemail.com>
2016-10-17 15:32:19 +1300

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.smxemail/re-frame-style-fx "0.0.1-SNAPSHOT"
+(defproject com.smxemail/re-frame-style-fx "0.0.1"
   :description "re-frame effects handlers for performing styling tasks."
   :url "https://github.com/SMX-LTD/re-frame-style-fx.git"
   :license {:name         "Eclipse Public License - v 1.0"
